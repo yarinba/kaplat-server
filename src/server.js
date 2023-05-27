c4568023f143d34c61d0c25308ab595526e356b1
Yarin Barnes <yarinb7877@gmail.com>
2023-05-27 19:49:22 +0300

    check railway env provided vars

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const port = 9583;
 
 // Run the server!
 app.listen({ port }, function (err, address) {
+  console.log("process.env", process.env);
   if (err) {
     app.log.error(err);
     process.exit(1);
